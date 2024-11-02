f9f6dd2775b2873bc01b4648849f48f3281a43dc
Nadia <nadiya.zhurakivska@gmail.com>
2024-11-02 15:24:16 +0100

    Add unit tests using xUnit and NSubstitute

---
+++ CarRental.API.Test/Controllers/CustomerControllerTest.cs
@@ -0,0 +1,150 @@
+﻿using AutoMapper;
+using CarRental.API.Controllers;
+using CarRental.API.Models;
+using CarRental.API.Profiles;
+using CarRental.API.Repositories;
+using CarRental.API.Repositories.Interfaces;
+using Microsoft.AspNetCore.Mvc;
+using NSubstitute;
+
+namespace CarRental.API.Test.Controllers
+{
+    public class CustomerControllerTest
+    {
+        [Fact]
+        public async Task GetAllCustomersAsync_RepositoryNotMocked_ExpectedDataFromStaticCollection()
+        {
+            //Arrange
+            var customerRepository = new CustomerRepository();
+            var mappingConfig = new MapperConfiguration(cfg =>
+            {
+                cfg.AddProfile<CustomerProfile>();
+            });
+
+            var mapper = mappingConfig.CreateMapper();
+            var controller = new CustomerController(customerRepository, mapper);
+
+            //Act
+            var result = await controller.GetAllCustomersAsync();
+
+            //Assert
+
+            //Assert.True(result.Any());
+            Assert.IsType<OkObjectResult>(result);
+            var okResult = result as OkObjectResult;
+            Assert.NotNull(okResult);
+
+            var customers = okResult.Value as IEnumerable<Customer>;
+            Assert.NotNull(customers);
+            Assert.True(customers.Any());
+        }
+
+        [Fact]
+        public async Task GetAllCustomersAsync_RepositoryIsMocked_ExpectedDataFromStaticCollection()
+        {
+            //Arrange
+            var customerRepository = Substitute.For<ICustomerRepository>();
+
+            var expectedCustomerList = new List<Customer>()
+            {
+                new Customer()
+                {
+                    CustomerID = 1,
+                    FirstName = "John",
+                    LastName = "Doe"
+                }
+            };
+            customerRepository.GetCustomersAsync().Returns(expectedCustomerList);
+
+            var mapper = Substitute.For<IMapper>();
+
+            var controller = new CustomerController(customerRepository, mapper);
+
+            //Act
+            var result = await controller.GetAllCustomersAsync();
+
+            //Assert
+            Assert.IsType<OkObjectResult>(result);
+            var okResult = result as OkObjectResult;
+            Assert.NotNull(okResult);
+
+            var customers = okResult.Value as IEnumerable<Customer>;
+            Assert.NotNull(customers);
+            Assert.True(customers.Any());
+            Assert.Equal("John", customers.ElementAt(0).FirstName);
+        }
+
+        [Fact]
+        public async Task GetCustomerByIdAsync_RepositoryNotMocked_ExpectedCustomerById()
+        {
+            //Arrange
+            var customerRepository = new CustomerRepository();
+            var mappingConfig = new MapperConfiguration(cfg =>
+            {
+                cfg.AddProfile<CustomerProfile>();
+            });
+
+            var mapper = mappingConfig.CreateMapper();
+            var controller = new CustomerController(customerRepository, mapper);
+
+            //Act
+            var result = await controller.GetCustomerByIdAsync(1);
+
+            //Assert
+            Assert.IsType<OkObjectResult>(result);
+            var okResult = result as OkObjectResult;
+            Assert.NotNull(okResult);
+
+            var customer = okResult.Value as Customer;
+            Assert.NotNull(customer);
+            Assert.Equal(1, customer.CustomerID);
+
+        }
+
+        [Fact]
+        public async Task GetCustomerByIdAsync_RepositoryIstMocked_ExpectedCustomerById()
+        {
+            //Arrange
+            var customerRepository = Substitute.For<ICustomerRepository>();
+            var expectedCustomer = new Customer()
+            {
+                CustomerID = 1,
+                FirstName = "John",
+                LastName = "Doe"
+            };
+
+            customerRepository.GetCustomerByIdAsync(1).Returns(expectedCustomer);
+            var mapper = Substitute.For<IMapper>();
+
+            var controller = new CustomerController(customerRepository, mapper);
+
+            //Act
+            var result = await controller.GetCustomerByIdAsync(1);
+
+            //Assert
+            Assert.IsType<OkObjectResult>(result);
+            var okResult = result as OkObjectResult;
+            Assert.NotNull(okResult);
+
+            var customer = okResult.Value as Customer;
+            Assert.NotNull(customer);
+            Assert.Equal(1, customer.CustomerID);
+        }
+
+        //[Fact]
+        //public async Task ClearCache_CustomerClearCache_WasCalledOnce()
+        //{
+        //    //Arrange
+        //    var customerRepository = Substitute.For<ICustomerRepository>();
+        //    var mapper = Substitute.For<IMapper>();
+        //
+        //    var controller = new CustomerController(customerRepository, mapper);
+        //
+        //    //Act
+        //    await controller.ClearCache();
+        //
+        //    //Assert
+        //    customerRepository.Received(1).ClearCashe();
+        //}
+    }
+}
---
+++ CarRental.API.Test/Repository/CustomerRepositoryTest.cs
@@ -0,0 +1,265 @@
+﻿using CarRental.API.Models;
+using CarRental.API.Repositories;
+
+namespace CarRental.API.Test.Repository
+{
+    public class CustomerRepositoryTest
+    {
+        [Fact]
+        public async Task GetCustomersAsync_NewlyIntializedStaticListWithTwoUsers_returnsTwoUsers()
+        {
+            //Arrange
+            CustomerRepository.Customers.Clear();
+            CustomerRepository.Customers =
+            [
+                new Customer
+                {
+                    CustomerID = 1,
+                    FirstName = "Ken",
+                    LastName = "Tamburello",
+                    Phone = "0897-012345",
+                    Address = null,
+                    User =  new User
+                    {
+                        UserID = 1,
+                        Email = "Ken.Tamburello@gmail.com",
+                        Password = "CCKOT"
+                    }
+                },
+                new Customer
+                {
+                    CustomerID = 2,
+                    FirstName = "Terri",
+                    LastName = "Walters",
+                    Phone = "(91) 567 8901",
+                    Address = new Address
+                    {
+                        AddressID = 2,
+                        Country = "Spain",
+                        City ="Madrid",
+                        Street = "Gran Vía, 4567",
+                        PostalCode= "10071"
+                    },
+                    User =  new User
+                    {
+                        UserID = 2,
+                        Email = "Terri.Walters@gmail.com",
+                        Password = "SIUIH"
+                    }
+                }
+            ];
+
+            var repository = new CustomerRepository();
+
+            //Act
+            List<Customer> result = await repository.GetCustomersAsync();
+
+            //Assert
+            Assert.Equal(2, result.Count);
+            Assert.Equal("Ken", result[0].FirstName);
+            Assert.Equal("Tamburello", result[0].LastName);
+            Assert.Null(result[0].Address);
+            Assert.NotNull(result[0].User);
+        }
+
+        [Fact]
+        public async Task GetCustomerByIdAsync_ShoudBeReturnCustomer_True()
+        {
+            //Arrange
+            CustomerRepository.Customers.Clear();
+            CustomerRepository.Customers =
+            [
+                new Customer
+                {
+                    CustomerID = 1,
+                    FirstName = "Ken",
+                    LastName = "Tamburello",
+                    Phone = "0897-012345",
+                    Address = new Address
+                    {
+                        AddressID = 1,
+                        Country = "Switzerland",
+                        City ="Genève",
+                        Street = "Grenzacherweg 0123",
+                        PostalCode= "10122"
+                    },
+                    User =  new User
+                    {
+                        UserID = 1,
+                        Email = "Ken.Tamburello@gmail.com",
+                        Password = "CCKOT"
+                    }
+                }
+            ];
+
+            //var repositoryMock = new Mock<ICustomerRepository>(); +using Moq
+            var repository = new CustomerRepository();
+
+            //Act
+            Customer result = await repository.GetCustomerByIdAsync(1);
+
+            //Assert
+            Assert.NotNull(result);
+        }
+
+        [Fact]
+        public async Task UpdateCustomerAsync_ShoudUpdateCustomer_ReturnNotNull()
+        {
+            //Arrange
+            CustomerRepository.Customers.Clear();
+            CustomerRepository.Customers =
+            [
+                new Customer
+                {
+                    CustomerID = 1,
+                    FirstName = "Ken",
+                    LastName = "Tamburello",
+                    Phone = "0897-012345",
+                    Address = new Address
+                    {
+                        AddressID = 1,
+                        Country = "Switzerland",
+                        City ="Genève",
+                        Street = "Grenzacherweg 0123",
+                        PostalCode= "10122"
+                    },
+                    User =  new User
+                    {
+                        UserID = 1,
+                        Email = "Ken.Tamburello@gmail.com",
+                        Password = "CCKOT"
+                    }
+                }
+            ];
+
+            var updateCustomer = new Customer
+            {
+                CustomerID = 1,
+                FirstName = "Terri",
+                LastName = "Walters",
+                Phone = "(91) 567 8901",
+                Address = new Address
+                {
+                    AddressID = 1,
+                    Country = "Switzerland",
+                    City = "Genève",
+                    Street = "Grenzacherweg 0123",
+                    PostalCode = "10122"
+                },
+                User = new User
+                {
+                    UserID = 1,
+                    Email = "Ken.Tamburello@gmail.com",
+                    Password = "CCKOT"
+                }
+            };
+            var repository = new CustomerRepository();
+
+            //Act
+            Customer result = await repository.UpdateCustomerAsync(1, updateCustomer);
+
+            //Assert
+            Assert.NotNull(result);
+            Assert.Equal(1, result.CustomerID);
+            Assert.Equal("Terri", result.FirstName);
+            Assert.Equal(updateCustomer.LastName, result.LastName);
+            Assert.Equal(CustomerRepository.Customers[0].CustomerID, result.CustomerID);
+        }
+
+        [Fact]
+        public async Task UpdateCustomerAsync_NotExistCustomer_()
+        {
+            //Arrange
+            CustomerRepository.Customers.Clear();
+            CustomerRepository.Customers =
+            [
+                new Customer
+                {
+                    CustomerID = 1,
+                    FirstName = "Ken",
+                    LastName = "Tamburello",
+                    Phone = "0897-012345",
+                    Address = new Address
+                    {
+                        AddressID = 1,
+                        Country = "Switzerland",
+                        City ="Genève",
+                        Street = "Grenzacherweg 0123",
+                        PostalCode= "10122"
+                    },
+                    User =  new User
+                    {
+                        UserID = 1,
+                        Email = "Ken.Tamburello@gmail.com",
+                        Password = "CCKOT"
+                    }
+                }
+            ];
+
+            var updateCustomer = new Customer
+            {
+                CustomerID = 1,
+                FirstName = "Terri",
+                LastName = "Walters",
+                Phone = "(91) 567 8901",
+                Address = new Address
+                {
+                    AddressID = 1,
+                    Country = "Switzerland",
+                    City = "Genève",
+                    Street = "Grenzacherweg 0123",
+                    PostalCode = "10122"
+                },
+                User = new User
+                {
+                    UserID = 1,
+                    Email = "Ken.Tamburello@gmail.com",
+                    Password = "CCKOT"
+                }
+            };
+            var repository = new CustomerRepository();
+
+            //Act
+
+            //Assert
+            Assert.ThrowsAsync<ArgumentException>(() => repository.UpdateCustomerAsync(10, updateCustomer));
+        }
+        [Fact]
+        public async Task DeleteCustomerAsync_ShoudBeRemoveCustomer_ReturnNull()
+        {
+            //Arrange
+            CustomerRepository.Customers.Clear();
+            CustomerRepository.Customers =
+            [
+                new Customer
+                {
+                    CustomerID = 1,
+                    FirstName = "Ken",
+                    LastName = "Tamburello",
+                    Phone = "0897-012345",
+                    Address = new Address
+                    {
+                        AddressID = 1,
+                        Country = "Switzerland",
+                        City ="Genève",
+                        Street = "Grenzacherweg 0123",
+                        PostalCode= "10122"
+                    },
+                    User =  new User
+                    {
+                        UserID = 1,
+                        Email = "Ken.Tamburello@gmail.com",
+                        Password = "CCKOT"
+                    }
+                }
+            ];
+            var repository = new CustomerRepository();
+            //Act
+            await repository.DeleteCustomerAsync(1);
+            var result = await repository.GetCustomerByIdAsync(1);
+
+            //Assert
+            Assert.Null(result);
+        }
+    }
+}
